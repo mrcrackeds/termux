c78e114e6f506809efe6726a84e626bf1cacc275
icammaci <59759267+icammaci@users.noreply.github.com>
2020-07-25 16:03:16 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,124 @@
+apt update && aptupgrade -y
+pkg install figlet -y
+pkg install toilet -y
+pkg install python -y
+pkg install python2 -y
+pkg install curl -y
+pkg install perl -y
+pip2 install --upgrade pip
+pkg install ruby -y
+gem install lolcat -y
+
+clear
+toilet -f smslant -F gay 'TERMUX PACKAGE INSTALLER BY MR CRACKED'
+sleep 3
+echo "\033[33;1m Texmux Package Installer" "\033[37;1m From" "\033[36;1mMahiska Cyber Team"
+
+pkg install figlet -y
+pkg install toilet -y
+pkg install python -y
+pkg install python2 -y
+pkg install curl
+pkg install perl
+pip2 install --upgrade pip
+pkg install ruby
+gem install lolcat
+
+clear
+toilet -f smslant -F gay 'TERMUX PACKAGE INSTALLER BY MAHISKA CYBER TEAM'
+sleep 1
+echo "\033[33;1m Termux Package Installer" "\033[37;1m From" "\033[36;1mMahiska Cyber Team"
+sleep 1
+echo "\033[31;1m Subscribe channel YouTube" "\033[36;1mMR CRACKED"
+echo " "
+echo "____________________________________________________________"|lolcat
+echo "\033[30;1m                 Syarat" "\033[34;1m&" "\033[30;1mKetentuan            "
+sleep 1
+echo "\033[32;1mPastikan Anda Tidak Pernah Menginstall bahan-bahan Sebelumnya"
+sleep 1
+echo "\033[32;1mSisakan Ruang Penyimpanan Minimal 1 GB"
+sleep 1
+echo "\033[32;1mJaringan Anda Harus Dalam Keadaan Online dan stabil"
+sleep 1
+echo "\033[32;1mAnda Harus Sabar Karena Proses Sangat Lama"
+sleep 1
+echo "\033[31;1mDilarang MeRecode Tools Ini"
+sleep 1
+echo "\033[31;1mJangan Sampai Layar Ponsel Anda Mati"
+sleep 1
+echo "\033[31;1mJangan Keluar Dari Aplikasi Termux"
+sleep 1
+echo "\033[31;1mJangan Lupa Suscribe Channel YouTube Kami"
+echo "____________________________________________________________"|lolcat
+sleep 3
+echo "\033[35;1m SUBSCRIBE MY CHANNEL MR CRACKED"
+echo "------------------------------------------------------------"|lolcat
+read -p "Siap? [Y/T]=" MR_CRACKED
+if [ $MR_CRACKED = Y ] || [ $MR_CRACKED = y ]
+then
+echo "\033[32;1mProses Penginstalan Package  Yang Dibutuhkan"
+apt update && apt upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install nodejs -y
+pkg install bash -y
+pkg install openssh -y
+pkg install figlet -y
+pkg install fish -y
+pkg install toilet -y
+pkg install ruby -y
+pkg install curl -y
+pkg install perl -y
+pkg install git -y
+pkg install php -y
+pkg install nano -y
+pkg install sl -y
+pkg install cmatrix -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+gem install bundler
+pkg install clang -y
+pkg install wget -y
+pkg install make -y
+pkg install openssl -y
+pkg install vim -y
+pkg install apache -y
+pkg install apache2 -y
+pkg install mpv -y
+pip install requests
+pip install mechanize
+pip2 install requests
+pip2 install mechanize
+pkg install moon-buggy
+pkg install mechanize -y
+pkg install lolcat -y
+pkg install neofetch -y
+pkg install cowsay -y
+pkg install gem -y
+apt install php -y
+apt install jq -y
+apt install tail -y
+apt install curl -y
+apt install zip -y
+pkg install wget -y
+echo "\033[32;1m INSTALASI SELESAI"
+echo "\033[33;1m Author: " "\033[34;1mMR CRACKED"
+sleep 1
+echo "Pengistalan bahan-bahan selesai"
+echo "Subscribe My Channel MR CRACKED"
+sleep 2
+echo "Apabila Ada kesalahan Hubungi Author : MR.CRACKED"
+echo "Akun github pembuat script : https://github.com/icammaci"
+echo "Akun youtube pembuat script : MR CRACKED"
+sleep 3
+echo "Akun instagram pembuat script : https://instagram.com/cracked_212"
+echo "Akun twitter pembuat script : @MrCracked3"
+sleep 4
+fi
+
+if [ $MR_CRACKED = T ] || [ $MR_CRACKED = t ]
+then
+echo "Mungkin Lain Kali"
+exit
+fi 
+
